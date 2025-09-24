248f84fde898cfb378909a44369ec7e3e602be1f
Jan De Smet <jan@printasy.com>
2025-09-24 23:20:29 +0200

    javascript2025

--- Oefenbundel/Oef20/js/index.js
+++ Oefenbundel/Oef20/js/index.js
@@ -1,13 +1,14 @@
-// Print het omgekeerde van een getal vb 1234 wordt 4321
+// Print priemgetallen tussen 1 & 100
 
-var mijnTekst = window.prompt("Geef een getal dat bestaat uit meerdere cijfers");
-
-var aantalKarakters = mijnTekst.length;
-
-for (var i=1; i<=aantalKarakters;i=i+1);
-
-tekst = tekst + mijnTekst[i];
-
-console.log (aantalKarakters);
-
-// test tekst ivm push naar github
+var isPriemGetal = 0;
+for (var getal = 2; getal <= 100; getal = getal + 1) {
+    for (var i = 1; i <= getal; i = i + 1) {
+        if (getal % i == 0) {
+            isPriemGetal = isPriemGetal + 1
+        };
+    }
+    if (isPriemGetal==2){
+        console.log(`${getal} is een priemgetal`)
+    };
+    isPriemGetal = 0;
+}
---
+++ Oefenbundel/Oef21/js/index.js
@@ -0,0 +1,13 @@
+// Print het omgekeerde van een getal vb 1234 wordt 4321
+
+var mijnTekst = window.prompt("Geef een getal dat bestaat uit meerdere cijfers");
+
+var aantalKarakters = mijnTekst.length;
+
+for (var i=1; i<=aantalKarakters;i=i+1);
+
+tekst = tekst + mijnTekst[i];
+
+console.log (aantalKarakters);
+
+// test tekst ivm push naar github
